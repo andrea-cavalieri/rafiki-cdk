017747b620721798d06484a3cdd31a17227dbff0
andrea.cavalieri@addvalue.it <andrea.cavalieri@addvalue.it>
2025-03-02 23:42:28 +0100

    Initial commit

---
+++ test/rafiki-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RafikiCdk from '../lib/rafiki-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/rafiki-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RafikiCdk.RafikiCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
